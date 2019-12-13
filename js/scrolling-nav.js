a4ddd40c086eb006eb8ae713e60eada99213b9c4
Jonathan Liu <liujon23@gmail.com>
2019-12-12 21:43:29 -0800

    Update scrolling-nav.js

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -9,7 +9,7 @@
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 800, "easeInOutExpo");
+        }, 800);
         return false;
       }
     }
