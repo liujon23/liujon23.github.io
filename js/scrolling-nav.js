72df21069d6a024b25d1e9f4fc3b54abb38f0e80
Jonathan Liu <liujon23@gmail.com>
2019-12-12 21:35:08 -0800

    Scrolls faster

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -8,8 +8,8 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 56)
-        }, 1000, "easeInOutExpo");
+          scrollTop: target.offset().top
+        }, 800, "easeInOutExpo");
         return false;
       }
     }
